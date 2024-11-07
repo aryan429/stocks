e93c35e7ff817f83da2c8ca25d3479ceb6d5595a
aryan_21 <aryan@in-sync.co.in>
2024-11-07 23:57:27 +0530

    first commit

---
+++ backend/Migrations/20241025153017_identity.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class identity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9b6ea52f-9add-4f8f-b5ed-6d9e376eb0ee");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f4f9c929-b952-4a27-91be-15174f56ea10");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "4efe4046-915c-4baf-85cf-df284b236fe4", null, "User", "USER" },
+                    { "e21379c6-dfb3-4b3c-90b6-32c532b578c1", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4efe4046-915c-4baf-85cf-df284b236fe4");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e21379c6-dfb3-4b3c-90b6-32c532b578c1");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "9b6ea52f-9add-4f8f-b5ed-6d9e376eb0ee", null, "Admin", "ADMIN" },
+                    { "f4f9c929-b952-4a27-91be-15174f56ea10", null, "User", "USER" }
+                });
+        }
+    }
+}
---
+++ backend/Migrations/20241025153126_SeedRole.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4efe4046-915c-4baf-85cf-df284b236fe4");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e21379c6-dfb3-4b3c-90b6-32c532b578c1");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "54d40849-c8e7-4dca-a227-166942c82fba", null, "User", "USER" },
+                    { "5b73942c-0333-4f05-ad94-cf97b5ccf456", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "54d40849-c8e7-4dca-a227-166942c82fba");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5b73942c-0333-4f05-ad94-cf97b5ccf456");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "4efe4046-915c-4baf-85cf-df284b236fe4", null, "User", "USER" },
+                    { "e21379c6-dfb3-4b3c-90b6-32c532b578c1", null, "Admin", "ADMIN" }
+                });
+        }
+    }
+}
